fae56217d69dc9603731549fa504689d934a09df
AyaKhalafallah <ayakhalafallah984@gmail.com>
2022-12-02 07:20:37 +0200

    j

---
+++ js/script.js
@@ -0,0 +1,16 @@
+for(var x=0;x<=100;x++)
+{
+	console.log(x)
+}
+var btn1=document.getElementById("btn1")
+var div1=document.getElementById("div1")
+function sum()
+{div1.innerHTML="hi"}
+btn1.onclick=sum
+
+
+
+
+
+
+
